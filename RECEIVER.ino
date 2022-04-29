3ad505910601840fc0d05a476fff9be696790b70
lle2022 <74795368+lle2022@users.noreply.github.com>
2022-04-28 21:29:08 -0400

    Add files via upload

--- RECEIVER.ino
+++ RECEIVER.ino
@@ -2,13 +2,11 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <LiquidCrystal.h>
 
 int ReceivedMessage[1] = {000};
 RF24 radio(10, 2);
-const byte address[6] = "00001";
-
-LiquidCrystal lcd(A0, 9, A3, A2, A1, 8);
+const byte address[6] = "00100";
+char POWER[8] = "";
 
 //Defining the State of Charge Board
 Adafruit_LC709203F lc;
@@ -17,14 +15,9 @@ Adafruit_LC709203F lc;
 #define Motor1P2 4
 #define Motor2P2 5
 #define Motor2P1 6
-#define Full 7
-#define Half 8
-#define Charge 9
+#define On 8
 
 void setup() {
-  
-  lcd.begin(16, 2);
-  lcd.print("Battery Power:");
   
   radio.begin();
 
@@ -54,163 +47,105 @@ void setup() {
   pinMode(Motor1P2, OUTPUT);
   pinMode(Motor2P1, OUTPUT);
   pinMode(Motor2P2, OUTPUT);
-  pinMode(Full,OUTPUT);
-  pinMode(Half,OUTPUT);
-  pinMode(Charge,OUTPUT);
+  pinMode(On,OUTPUT);
 }
 
 void loop() {
-
-//Sees if there is radio available to receive information
   while (radio.available()) {
-
+    // If Radio Pipe Open = Controller Communicating
+    digitalWrite(On, HIGH);
     radio.read(ReceivedMessage, 1);
-
-// Obtains 3-bit message to see what input the controller sent to change the current through the motors
 
     if (ReceivedMessage[0] == 001) {
       // Both Motors Running Forward Fast
-      
-      Serial.println("Forward");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      batterycheck(lc.cellPercent());
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
       digitalWrite(Motor1P1, HIGH);
       digitalWrite(Motor1P2, LOW);
       digitalWrite(Motor2P1, HIGH);
       digitalWrite(Motor2P2, LOW);
       analogWrite(Motor1P1,255);
       analogWrite(Motor2P1,255);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
     
     if (ReceivedMessage[0] == 010) {
       // Both Motors Running Forward Slower
-
-      Serial.println("Forward Slow");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
+      digitalWrite(Motor1P1, HIGH);
+      digitalWrite(Motor1P2, LOW);
+      digitalWrite(Motor2P1, HIGH);
+      digitalWrite(Motor2P2, LOW);
       analogWrite(Motor1P1,125);
       analogWrite(Motor2P1,125);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
     
     if (ReceivedMessage[0] == 000) {
       // Both Motors Off
-      
-      Serial.println("Off");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      batterycheck(lc.cellPercent());
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
       digitalWrite(Motor1P1, LOW);
       digitalWrite(Motor1P2, LOW);
       digitalWrite(Motor2P1, LOW);
       digitalWrite(Motor2P2, LOW);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
 
     if (ReceivedMessage[0] == 100){
       // Both Motors Running Reverse Fast
-      
-      Serial.println("Reverse");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      batterycheck(lc.cellPercent());
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
       digitalWrite(Motor1P1, LOW);
       digitalWrite(Motor1P2, HIGH);
       digitalWrite(Motor2P1, LOW);
       digitalWrite(Motor2P2, HIGH);
       analogWrite(Motor1P2,255);
       analogWrite(Motor2P2,255);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
     
     if (ReceivedMessage[0] == 011) {
       // Both Motors Running Reverse Slower
-
-      Serial.println("Reverse Slow");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
+      digitalWrite(Motor1P1, LOW);
+      digitalWrite(Motor1P2, HIGH);
+      digitalWrite(Motor2P1, LOW);
+      digitalWrite(Motor2P2, HIGH);
       analogWrite(Motor1P2,125);
       analogWrite(Motor2P2,125);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
 
     if (ReceivedMessage[0] == 101){
       // TURN LEFT
-      
-      Serial.println("TURN LEFT");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      batterycheck(lc.cellPercent());
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
       digitalWrite(Motor1P1, LOW);
       digitalWrite(Motor1P2, HIGH);
       digitalWrite(Motor2P1, HIGH);
       digitalWrite(Motor2P2, LOW);
       analogWrite(Motor1P2,255);
       analogWrite(Motor2P1,255);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
 
     if (ReceivedMessage[0] == 110){
       // TURN RIGHT
-      
-      Serial.println("TURN RIGHT");
-      Serial.print("Batt Percent: "); Serial.println(lc.cellPercent(), 1);
-      batterycheck(lc.cellPercent());
-      lcd.setCursor(0, 1);
-      lcd.print(lc.cellPercent(), 1);
       digitalWrite(Motor1P1, HIGH);
       digitalWrite(Motor1P2, LOW);
       digitalWrite(Motor2P1, LOW);
       digitalWrite(Motor2P2, HIGH);
       analogWrite(Motor1P1,255);
       analogWrite(Motor2P2,255);
-
-      transmitter(address,lc.cellPercent(), ReceivedMessage[0]);
     }
 
-  }
-}
+    if (ReceivedMessage[0] == 111) {
+      radio.begin();
+      radio.openWritingPipe(address);
+      radio.stopListening();
 
-void batterycheck(float charge) {
-  // Green LED if above 50%
-  // Yellow LED if between 50 and 10%
-  // Red LED if below 10%
-  if(charge > 50.0) {
-    digitalWrite(Full,1);
-    digitalWrite(Half,0);
-    digitalWrite(Charge,0);
-  } else if(charge > 10.0) {
-    digitalWrite(Full,0);
-    digitalWrite(Half,1);
-    digitalWrite(Charge,0);
-  } else  {
-    digitalWrite(Full,0);
-    digitalWrite(Half,0);
-    digitalWrite(Charge,1);
+      dtostrf(lc.cellPercent(), 6, 2, POWER);
+      radio.write(&POWER, sizeof(POWER));
+
+      radio.begin();
+      radio.openReadingPipe(0, address);
+      radio.startListening();
   }
+
+  }
+  digitalWrite(On, LOW);
 }
 
 void transmitter(const byte stuff, float batt, int msg[0]) {
-
-// Switches receiving to transmit to send the vehicle battery life
-
   char battery[8] = "";
   
   if (msg[0] == 111) {
+    Serial.println("RECEIVED");
     radio.begin();
     radio.openWritingPipe(stuff);
     radio.stopListening();
